8438c608ee06a60f1ddf43e7ff887df446a10b56
alcesleo <lagginglion@gmail.com>
2013-12-04 21:40:54 +0100

    Prettified all the PHP files
    This code was seriously fucked up, so to be able to work with it at all I've run
    a bunch of scripts on it. The indentation has been fixed, and converted
    to 4 spaces everywhere. PHP-CS-Fixer has made the coding standard just a tiny bit
    less horrible.

--- src/check.php
+++ src/check.php
@@ -6,14 +6,13 @@ $u = $_POST['username'];
 $p = $_POST['password'];
 
 // Check if user is OK
-if(isUser($u, $p)) {
-	// set the session
-	sec_session_start();
-	$_SESSION['login_string'] = hash('sha512', "Come_On_You_Spurs" +$u); 
-	$_SESSION['user'] = $u;
-	header("Location: img/middle.php");
-}
-else {
-	// To bad
-	header('HTTP/1.1 401 Unauthorized');
+if (isUser($u, $p)) {
+    // set the session
+    sec_session_start();
+    $_SESSION['login_string'] = hash('sha512', "Come_On_You_Spurs" +$u);
+    $_SESSION['user'] = $u;
+    header("Location: img/middle.php");
+} else {
+    // To bad
+    header('HTTP/1.1 401 Unauthorized');
 }
